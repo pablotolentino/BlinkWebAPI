d96b8f020987197e751371c7cd75dadb6fe27ac0
Pablo Tolentino <pablotolentino@outlook.com>
2018-05-19 12:52:09 -0500

    versión inicial del proyecto de Provident web api

---
+++ BlinkWebApi/Blink.WebApi/Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blink.WebApi.Areas.Account.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
